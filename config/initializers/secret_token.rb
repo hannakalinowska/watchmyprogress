c1b6f78d6a0cccc6147361ca7e1253564e6d0b58
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-05-16 21:39:33 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watchmyprogress::Application.config.secret_token = 'd55a24bf5f5bbd77e87edb9247929ab43525043ca081a3f69216f78edf287b31701ef81cf4d65eba81f90a0cfa2fef4cdf41f987cc12389b7fb3f929953f2e97'
