8a9a83d85439f3d5f1789097392ca7d67fafa3ad
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-06-18 20:10:48 +0100

    created a User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110516205839) do
+ActiveRecord::Schema.define(:version => 20110618190647) do
 
   create_table "projects", :force => true do |t|
     t.integer  "user_id"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(:version => 20110516205839) do
     t.text     "description"
     t.datetime "starts_at"
     t.datetime "ends_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
